ac23bb4f8d322bc93dfec5aab4b8312ca9c3ce01
Tomas <tmercadoslp@gmail.com>
2024-12-04 11:00:38 -0300

    experiments with date fns tz

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log("Hello world!");
+import { format } from "date-fns";
+import { fromZonedTime, toZonedTime } from "date-fns-tz";
+
+const USER_ZONE = "America/Bogota";
+
+const eventInputDateStr = "2024-01-01";
+const eventInputTimeStr = "12:00:00";
+const eventInputZone = "Asia/Tokyo";
+
+const eventInputDateTimeStr = `${eventInputDateStr}T${eventInputTimeStr}`;
+
+const eventUTCDate = fromZonedTime(eventInputDateTimeStr, eventInputZone);
+const eventInPlaceDate = toZonedTime(eventUTCDate, eventInputZone);
+const eventUserDate = toZonedTime(eventUTCDate, USER_ZONE);
+
+console.log(
+  `In place event date: ${format(eventInPlaceDate, "yyyy-MM-dd HH:mm:ss")}`,
+);
+console.log(
+  `Event date in user's time zone: ${format(
+    eventUserDate,
+    "yyyy-MM-dd HH:mm:ss",
+  )}`,
+);
