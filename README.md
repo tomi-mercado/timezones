c6e49edbd1aba3aa6b57dacc449a4986145a15f8
Tomas <tmercadoslp@gmail.com>
2024-12-05 08:33:04 -0300

    add readme, comments and another user zone to be more clear

---
+++ README.md
@@ -0,0 +1,16 @@
+# Timezone experiments
+
+Some experiments with timezones in TypeScript with date-fns and date-fns-tz.
+This explain how based on a timezone and a date in which an event take place, we can calculate the time in another timezone.
+
+## Setup
+
+```bash
+bun install
+```
+
+## Run
+
+```bash
+bun dev
+```
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,40 @@
 import { format } from "date-fns";
 import { fromZonedTime, toZonedTime } from "date-fns-tz";
 
-const USER_ZONE = "America/Bogota";
+const USER_ZONE_A = "America/Bogota";
+const USER_ZONE_B = "America/Argentina/Buenos_Aires";
 
+// We need 3 fields in Salesforce.
+// One string with a regex for dates
 const eventInputDateStr = "2024-01-01";
+// One string with a regex for times
 const eventInputTimeStr = "12:00:00";
+// One picklist with timezones
 const eventInputZone = "Asia/Tokyo";
 
+// We need to combine the date and time strings
 const eventInputDateTimeStr = `${eventInputDateStr}T${eventInputTimeStr}`;
 
+// From the combined string, we obtain the UTC date
 const eventUTCDate = fromZonedTime(eventInputDateTimeStr, eventInputZone);
+
+// With this UTC, we can obtain the hour of the event in any place of the world
 const eventInPlaceDate = toZonedTime(eventUTCDate, eventInputZone);
-const eventUserDate = toZonedTime(eventUTCDate, USER_ZONE);
+const eventUserADate = toZonedTime(eventUTCDate, USER_ZONE_A);
+const eventUserBDate = toZonedTime(eventUTCDate, USER_ZONE_B);
 
 console.log(
   `In place event date: ${format(eventInPlaceDate, "yyyy-MM-dd HH:mm:ss")}`,
+); // 2024-01-01 12:00:00
+console.log(
+  `Event date in user A's time zone: ${format(
+    eventUserADate,
+    "yyyy-MM-dd HH:mm:ss",
+  )}`, // 2023-12-31 22:00:00
 );
 console.log(
-  `Event date in user's time zone: ${format(
-    eventUserDate,
+  `Event date in user B's time zone: ${format(
+    eventUserBDate,
     "yyyy-MM-dd HH:mm:ss",
-  )}`,
+  )}`, // 2023-12-31 00:00:00
 );
